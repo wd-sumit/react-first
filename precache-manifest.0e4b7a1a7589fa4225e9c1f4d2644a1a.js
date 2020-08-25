389f2b64acd8a78bcc8115df6d58f8f0d2df2af4
wd-sumit <wdsumit.pal@gmail.com>
2020-08-25 17:24:03 +0530

    Updates

---
+++ precache-manifest.0e4b7a1a7589fa4225e9c1f4d2644a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4a78bcb47a084035e7c856d8fc96c1",
+    "url": "/react-first/index.html"
+  },
+  {
+    "revision": "dfe61b4b39aaee7880d6",
+    "url": "/react-first/static/css/main.1ca0a567.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/react-first/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-first/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe61b4b39aaee7880d6",
+    "url": "/react-first/static/js/main.df3b6691.chunk.js"
+  },
+  {
+    "revision": "748c3939087206ccd66d",
+    "url": "/react-first/static/js/runtime-main.73dcb692.js"
+  }
+]);
